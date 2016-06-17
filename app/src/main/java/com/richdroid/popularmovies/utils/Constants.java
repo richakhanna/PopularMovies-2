45df29377a4c928e7dfe8f2cdfdd9d955dc387a7
Richa Khanna <richie.khanna10@gmail.com>
2016-06-17 15:16:05 +0530

    Made Constant class constructor private

--- app/src/main/java/com/richdroid/popularmovies/utils/Constants.java
+++ app/src/main/java/com/richdroid/popularmovies/utils/Constants.java
@@ -3,11 +3,18 @@ package com.richdroid.popularmovies.utils;
 /**
  * Created by richa.khanna on 5/2/16.
  */
-public class Constants {
+public final class Constants {
 
     public final static String MOST_POPULAR = "most_popular";
     public final static String HIGHEST_RATED = "highest_rated";
     public final static String FAVORITE = "favorite";
 
     public static final String ARG_MOVIE_DETAIL = "MOVIE_DETAIL";
+
+    // Suppress default constructor for noninstantiability
+    private Constants() {
+        throw new AssertionError("No Constants instances for you!");
+    }
 }
+
+
